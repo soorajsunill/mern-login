22819c2b1bd4578032c4095e07808d28e8f50e16
soorajsunill <soorajsunil9495@gmail.com>
2024-10-10 00:32:09 +0530

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-mongoose.connect("mongodb+srv://soorajsunil9495:soorajsunil9495@cluster0.vkzvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://soorajsunil9495:soorajsunil9495@cluster0.vkzvy.mongodb.net/perfumes?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
   console.log("connected to mongodb");
   
