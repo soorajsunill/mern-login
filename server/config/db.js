a7e9571da0512856fc49c93ae0f3dd0e9a423ad7
soorajsunill <soorajsunil9495@gmail.com>
2024-10-10 23:24:57 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-mongoose.connect("mongodb+srv://soorajsunil9495:soorajsunil9495@cluster0.vkzvy.mongodb.net/perfumes?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://soorajsunil9495:soorajsunil9495@cluster0.vkzvy.mongodb.net/perfumes")
 .then(()=>{
   console.log("connected to mongodb");
   
